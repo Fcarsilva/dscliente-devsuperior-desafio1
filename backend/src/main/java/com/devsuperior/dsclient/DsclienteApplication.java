88ed94e58b7394cdc828231a0842875700cb378a
Fábio Silva <atctecnologia@gmail.com>
2022-08-16 11:47:04 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dsclient/DsclienteApplication.java
+++ backend/src/main/java/com/devsuperior/dsclient/DsclienteApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dscliente;
+package com.devsuperior.dsclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
